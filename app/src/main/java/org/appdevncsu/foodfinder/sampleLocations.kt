9a21e2ab5564ebec08e5b9f7232ea2532b80ada6
Daniel <dflores2@ncsu.edu>
2025-10-09 18:55:04 -0400

    Added Colors

--- app/src/main/java/org/appdevncsu/foodfinder/sampleLocations.kt
+++ app/src/main/java/org/appdevncsu/foodfinder/sampleLocations.kt
@@ -1,14 +1,10 @@
 package org.appdevncsu.foodfinder
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.snapping.SnapPosition
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -20,8 +16,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -47,7 +41,7 @@ fun LocationList(locations: List<DiningLocation>) {
 fun LocationItem(location: DiningLocation) {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
         Card(modifier = Modifier.padding(vertical = 8.dp).size(width = 200.dp, height = 100.dp).clickable{},
-        colors = CardDefaults.cardColors(containerColor = Color(0xFFFF0000), contentColor = Color(0xFFFFFFFF)))
+        colors = CardDefaults.cardColors(containerColor = Color(0xFF911C1C), contentColor = Color(0xFFFFFFFF)))
         {
 
             Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
