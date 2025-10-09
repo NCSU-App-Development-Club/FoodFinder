28575df6f2e1e8594dc9eee67cc43b9d6bd9ea35
Daniel <dflores2@ncsu.edu>
2025-10-09 19:41:26 -0400

    Headline completed

--- app/src/main/java/org/appdevncsu/foodfinder/sampleLocations.kt
+++ app/src/main/java/org/appdevncsu/foodfinder/sampleLocations.kt
@@ -1,10 +1,14 @@
 package org.appdevncsu.foodfinder
 
+
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -16,6 +20,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,6 +42,27 @@ fun LocationList(locations: List<DiningLocation>) {
     } */
 }
 
+
+@Composable
+fun Top() {
+    Box(modifier = Modifier.fillMaxWidth().fillMaxHeight(.15f).background(Color.Red))
+    Column {
+        Text(
+            text = "Food finder",
+            color = Color.White,
+            modifier = Modifier.padding(horizontal = 10.dp, vertical = 20.dp),
+            fontSize = 30.sp
+            )
+        Text(
+            text = "NCSU Dining",
+            color = Color.White,
+            modifier = Modifier.padding(horizontal = 10.dp),
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Light
+
+        )
+    }
+}
 @Composable
 fun LocationItem(location: DiningLocation) {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
@@ -45,8 +71,9 @@ fun LocationItem(location: DiningLocation) {
         {
 
             Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+
+
                 Text(text = location.name, fontSize = 20.sp)
-                Text(text = "Unit ID: ${location.unitId}", fontSize = 20.sp)
 
             }
 
@@ -60,5 +87,6 @@ fun LocationItem(location: DiningLocation) {
 @Composable
 @Preview
 fun LocationLisPreview() {
-    LocationList(sampleLocations)
+    Top()
+    //LocationList(sampleLocations)
 }
