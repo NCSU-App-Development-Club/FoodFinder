2d3f9ad7c775a4de10ae23be0b6de49be1668526
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2025-11-06 19:26:40 -0500

    Add ViewModel for location list

--- app/src/main/java/org/appdevncsu/foodfinder/composables/LocationList.kt
+++ app/src/main/java/org/appdevncsu/foodfinder/composables/LocationList.kt
@@ -8,13 +8,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.statusBars
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -23,23 +21,25 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.appdevncsu.foodfinder.data.DiningLocation
-import org.appdevncsu.foodfinder.data.sampleLocations
+import org.appdevncsu.foodfinder.viewmodel.LocationListViewModel
 
 @Composable
-fun LocationList(locations: List<DiningLocation>) {
+fun LocationList() {
+    val viewModel: LocationListViewModel = viewModel()
+    val locations by viewModel.locations.collectAsState()
     Column {
         LazyColumn(modifier = Modifier.fillMaxSize()) {
             items(locations) { location ->
@@ -101,5 +101,5 @@ fun LocationItem(location: DiningLocation, modifier: Modifier = Modifier) {
 @Composable
 @Preview
 fun LocationListPreview() {
-    LocationList(sampleLocations)
+    LocationList()
 }
