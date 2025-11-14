e5a6c763e83b7b1a2b75372982a76872972845d8
Daniel <dflores2@ncsu.edu>
2025-11-13 19:49:15 -0500

    viewmodel fix

--- app/src/main/java/org/appdevncsu/foodfinder/viewmodel/MenuViewModel.kt
+++ app/src/main/java/org/appdevncsu/foodfinder/viewmodel/MenuViewModel.kt
@@ -1,12 +1,13 @@
 package org.appdevncsu.foodfinder.viewmodel
 
+import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.update
 import org.appdevncsu.foodfinder.data.DiningMenuSection
 import org.appdevncsu.foodfinder.data.sampleMenuSections
 
-class MenuViewModel {
+class MenuViewModel : ViewModel(){
 
     private val _sections : MutableStateFlow<List<DiningMenuSection>> = MutableStateFlow(emptyList())
 
