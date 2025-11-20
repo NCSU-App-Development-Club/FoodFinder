6838822a248657b3da7b7c17abbf9b72991b5d42
thee12 <94206486+thee12@users.noreply.github.com>
2025-11-20 15:17:06 -0500

    Menu view model (#17)
    * LocationList start of implementation
    
    * MenuViewModel
    
    * viewmodel fix
    
    * Remove extra file
    
    ---------
    
    Co-authored-by: Daniel <dflores2@ncsu.edu>
    Co-authored-by: FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>

---
+++ app/src/main/java/org/appdevncsu/foodfinder/viewmodel/MenuViewModel.kt
@@ -0,0 +1,19 @@
+package org.appdevncsu.foodfinder.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import org.appdevncsu.foodfinder.data.DiningMenuSection
+import org.appdevncsu.foodfinder.data.sampleMenuSections
+
+class MenuViewModel : ViewModel(){
+
+    private val _sections : MutableStateFlow<List<DiningMenuSection>> = MutableStateFlow(emptyList())
+
+    val sections : StateFlow<List<DiningMenuSection>> = _sections
+
+    init {
+        _sections.update { sampleMenuSections }
+    }
+}
