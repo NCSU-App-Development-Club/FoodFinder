7475894fa44a81ec19d3efa714110b89dc1c343d
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2026-04-03 00:32:12 -0400

    Log API calls

--- backend/src/main/kotlin/org/appdevncsu/foodfinder/server/Server.kt
+++ backend/src/main/kotlin/org/appdevncsu/foodfinder/server/Server.kt
@@ -4,6 +4,7 @@ import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.cio.*
 import io.ktor.server.engine.*
+import io.ktor.server.plugins.calllogging.*
 import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -34,6 +35,7 @@ fun Application.module() {
     install(ContentNegotiation) {
         json()
     }
+    install(CallLogging)
 }
 
 fun Application.configureRouting() {
