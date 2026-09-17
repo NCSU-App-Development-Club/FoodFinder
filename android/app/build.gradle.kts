f38d303322493b134e69d453c0bc727dfddc6c83
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2026-09-17 02:20:18 -0400

    Bump version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "org.appdevncsu.foodfinder"
         minSdk = 28
         targetSdk = 36
-        versionCode = 5
+        versionCode = 7
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
