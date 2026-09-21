61d2b3c51f3790351031de20a01354d48721ab0d
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2026-09-20 20:43:15 -0400

    Bump version and fix spacing issue on privacy policy

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         applicationId = "org.appdevncsu.foodfinder"
         minSdk = 28
         targetSdk = 36
-        versionCode = 7
+        versionCode = 8
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
